6e87e60462e41b0fa4c002a29e66fdcad4d56a8d
Laps-F <maurolaps@gmail.com>
2023-03-28 15:47:09 -0300

    Changing postId and Id on TS file

--- src/data.ts
+++ src/data.ts
@@ -23,12 +23,12 @@ class Post {
 
 class ListOfComments {
 
-    constructor(private post: Post, private postId: number, private email: string, private body: string){
+    constructor(private id: number, private postId: number, private email: string, private body: string){
 
     }
 
-    get Post(){
-        return this.post;
+    get Id(){
+        return this.id;
     }
 
     get PostId(){
@@ -49,10 +49,10 @@ const second_post = new Post(2,"Lorem ipsum dolor sit amet, consectetur adipisci
 const third_post = new Post(3,"Lorem ipsum dolor sit amet, consectetur adipiscing elit.", "https://picsum.photos/200/300", "Etiam massa massa, laoreet ac neque vel, condimentum eleifend nunc. In luctus, metus nec consequat malesuada, odio nisi dapibus enim, rutrum aliquet ante quam vitae tortor.");
 const fourth_post = new Post(4,"Lorem ipsum dolor sit amet, consectetur adipiscing elit.", "https://picsum.photos/200/300", "Etiam massa massa, laoreet ac neque vel, condimentum eleifend nunc. In luctus, metus nec consequat malesuada, odio nisi dapibus enim, rutrum aliquet ante quam vitae tortor.");
 
-const first_comment = new ListOfComments(first_post, 10, "bibendum@email.com", "Fusce ipsum mi, molestie at ante nec, viverra egestas purus. In eu lectus at augue consectetur venenatis sit amet sed mauris.");
-const second_comment = new ListOfComments(first_post, 20, "maximus@email.com", "Fusce ipsum mi, molestie at ante nec, viverra egestas purus. In eu lectus at augue consectetur venenatis sit amet sed mauris.");
-const third_comment = new ListOfComments(first_post, 30, "consectetur@email.com", "Fusce ipsum mi, molestie at ante nec, viverra egestas purus. In eu lectus at augue consectetur venenatis sit amet sed mauris.");
-const fourth_comment = new ListOfComments(first_post, 40, "vulputate@email.com", "Fusce ipsum mi, molestie at ante nec, viverra egestas purus. In eu lectus at augue consectetur venenatis sit amet sed mauris.");
+const first_comment = new ListOfComments(10, first_post.Id, "bibendum@email.com", "Fusce ipsum mi, molestie at ante nec, viverra egestas purus. In eu lectus at augue consectetur venenatis sit amet sed mauris.");
+const second_comment = new ListOfComments(20, first_post.Id, "maximus@email.com", "Fusce ipsum mi, molestie at ante nec, viverra egestas purus. In eu lectus at augue consectetur venenatis sit amet sed mauris.");
+const third_comment = new ListOfComments(30, first_post.Id, "consectetur@email.com", "Fusce ipsum mi, molestie at ante nec, viverra egestas purus. In eu lectus at augue consectetur venenatis sit amet sed mauris.");
+const fourth_comment = new ListOfComments(40, first_post.Id, "vulputate@email.com", "Fusce ipsum mi, molestie at ante nec, viverra egestas purus. In eu lectus at augue consectetur venenatis sit amet sed mauris.");
 
 document.getElementById("first-title")!.innerHTML = first_post.Title;
 document.getElementById("second-title")!.innerHTML = second_post.Title;
@@ -63,7 +63,6 @@ document.getElementById("fourth-title")!.innerHTML = fourth_post.Title;
 (document.getElementById("second-img")! as HTMLImageElement).src = second_post.ImageURL;
 (document.getElementById("third-img")! as HTMLImageElement).src = second_post.ImageURL;
 (document.getElementById("fourth-img")! as HTMLImageElement).src = second_post.ImageURL;
-
 
 document.getElementById("first-body")!.innerHTML = first_post.Body;
 document.getElementById("second-body")!.innerHTML = second_post.Body;
